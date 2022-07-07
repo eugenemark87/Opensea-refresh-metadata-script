881c13ace92811197c5d582fb5fb0199d9d57b03
eugene mark <eugenemark@live.com>
2022-07-07 12:30:02 +0800

    update

--- page-eater.js
+++ page-eater.js
@@ -2,8 +2,9 @@
 document.getElementsByClassName('sc-1xf18x6-0 sc-glfma3-0 hiIVBZ gyCmAw sc-1skvztv-0 fPnOUC')[0].click();
 
 
-  //this code will execute after 2 second (2000 milliseconds)
+//this code will execute after 2 second (2000 milliseconds)
 setTimeout(function() {
 
+//navigate to the next url
 window.location.href = "[link]" + (parseInt(window.location.href.split("/").pop()) + 1);
 }, 2000);
