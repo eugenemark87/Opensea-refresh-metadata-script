aeeaa29ce7d38348b45d659e4460f7098668a54f
eugene mark <eugenemark@live.com>
2022-07-07 11:40:26 +0800

    Initial Commit

---
+++ page-eater.js
@@ -0,0 +1,9 @@
+﻿//click refresh button
+document.getElementsByClassName('sc-1xf18x6-0 sc-glfma3-0 hiIVBZ gyCmAw sc-1skvztv-0 fPnOUC')[0].click();
+
+
+  //this code will execute after 2 second (2000 milliseconds)
+setTimeout(function() {
+
+window.location.href = "[link]/" + (parseInt(window.location.href.split("/").pop()) + 1);
+}, 2000);
